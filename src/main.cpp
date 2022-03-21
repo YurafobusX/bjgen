8ceecbf5774f10f2660af578735afa21331aaf5d
YurafobusX <YurafobusX@gmail.com>
2022-03-22 01:22:12 +0300

    add basic support of dynamic marks

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,4 @@
+#include <cstddef>
 #include <iostream>
 #include <istream>
 #include <ostream>
@@ -12,25 +13,35 @@
 //Вектор, хранящий замену для всех статических маркеров
 std::vector<std::string> staticMarks = {"test"};
 
+//Вектор, хранящий замену для всех динамических маркеров
+std::vector<std::vector<std::string>> dynamicMarks = {
+    {"1"},
+    {"2"}
+};
+
+//Индекс замены для динамических маркеров
+size_t count = 0;
+
 //Принимает на вход строку и пишет её в выводной поток, заменяя все маркеры (\[[S,D]number\]) на их значение
-int replace(std::stringstream& in, std::ostream& output = std::cout) {
+int replace(std::istream& in, std::ostream& output = std::cout) {
     char current;
 
     while(in >> current) {
         if (current =='[') {
             in >> current;
+            //TODO Может закончится файл
+            size_t number;
+            in >> number;
             if (current == 'S') {
-                int number;
-                //TODO Может закончится файл
-                in >> number;
                 output << staticMarks[number - 1];
-            } else {
-                //TODO Динамические маркеры
-            }
+            } else if (current == 'D') {
+                output << dynamicMarks[count][number - 1];
+            } else return -1;
             in >> current;
             if (current != ']') return -1;
         } else output << current;
     };
+    //in.seekg(0);
     return 0;
 };
 
@@ -38,18 +49,24 @@ int main(int argc, char* argv[]) {
     std::istream* input = &std::cin;
     std::ostream* output = &std::cout; 
 
-    std::ifstream finput("../resources/example.html", std::ios::in | std::ios::binary);
+    std::ifstream finput("../resources/example.html", std::ios::in);
     if (!finput.good()) 
         return -1;
     input = &finput;
+    *input >> std::noskipws;
 
     std::ofstream foutput("../resources/result.html", std::ios::out);
+    if (!foutput.good()) 
+        return -1;
     output = &foutput;
 
-    std::stringstream file;
-    file << std::noskipws << input->rdbuf();
+    std::string file((std::istreambuf_iterator<char>(*input)), std::istreambuf_iterator<char>());
 
-    replace(file, *output);
+    for (count = 0; count < dynamicMarks.size(); ++count) {
+        auto j = std::stringstream(file);
+        j << std::noskipws;
+        if (replace(j, *output)) return -1;
+    }
 
     return 0;
 };
