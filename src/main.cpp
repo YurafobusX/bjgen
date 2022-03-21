f9474e2a3e200df9411aa1b25df0e2dc7f5c3f35
YurafobusX <YurafobusX@gmail.com>
2022-03-22 00:02:25 +0300

    opt

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,18 @@
 #include <iostream>
+#include <ostream>
 #include <string>
 #include <map>
 #include <utility>
 #include <sstream>
 #include <vector>
 #include <stdexcept>
+#include <fstream>
 
 //Вектор, хранящий замену для всех статических маркеров
 std::vector<std::string> staticMarks = {"test"};
 
-//Принимает на вход строку и возращает копию, заменяя все маркеры (\[[S,D]number\]) на их значение
-std::string replace(std::stringstream& in) {
-    std::stringstream res;
-
+//Принимает на вход строку и пишет её в выводной поток, заменяя все маркеры (\[[S,D]number\]) на их значение
+int replace(std::stringstream& in, std::ostream& output = std::cout) {
     char current;
 
     while(in >> current) {
@@ -22,22 +22,22 @@ std::string replace(std::stringstream& in) {
                 int number;
                 //TODO Может закончится файл
                 in >> number;
-                res << staticMarks[number - 1];
+                output << staticMarks[number - 1];
             } else {
                 //TODO Динамические маркеры
             }
             in >> current;
             if (current != ']') throw std::runtime_error("");
-        } else res << current;
+        } else output << current;
     };
-    return res.str();
+    return 0;
 };
 
 int main(int argc, char* argv[]) {
 
     std::stringstream file = std::stringstream(std::string((std::istreambuf_iterator<char>(std::cin)), std::istreambuf_iterator<char>()));
 
-    std::cout << replace(file);
+    replace(file);
 
     return 0;
 };
