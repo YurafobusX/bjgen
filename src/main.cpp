0c1e22d0df30f68d52a6d23793c49df0bae2b2b0
YurafobusX <YurafobusX@gmail.com>
2022-03-22 20:35:58 +0300

    add basic xlsx support

--- src/main.cpp
+++ src/main.cpp
@@ -1,23 +1,18 @@
 #include <cstddef>
 #include <iostream>
-#include <istream>
-#include <ostream>
 #include <string>
 #include <map>
 #include <utility>
 #include <sstream>
 #include <vector>
-#include <stdexcept>
 #include <fstream>
+#include "OpenXLSX.hpp"
 
 //Вектор, хранящий замену для всех статических маркеров
 std::vector<std::string> staticMarks = {"test"};
 
 //Вектор, хранящий замену для всех динамических маркеров
-std::vector<std::vector<std::string>> dynamicMarks = {
-    {"1"},
-    {"2"}
-};
+std::vector<std::vector<std::string>> dynamicMarks;
 
 //Индекс замены для динамических маркеров
 size_t count = 0;
@@ -36,9 +31,9 @@ int replace(std::istream& in, std::ostream& output = std::cout) {
                 output << staticMarks[number - 1];
             } else if (current == 'D') {
                 output << dynamicMarks[count][number - 1];
-            } else return -1;
+            } else return 2;
             in >> current;
-            if (current != ']') return -1;
+            if (current != ']') return 2;
         } else output << current;
     };
     //in.seekg(0);
@@ -48,24 +43,44 @@ int replace(std::istream& in, std::ostream& output = std::cout) {
 int main(int argc, char* argv[]) {
     std::istream* input = &std::cin;
     std::ostream* output = &std::cout; 
-    
+
     std::ifstream finput("../resources/example.html", std::ios::in);
     if (!finput.good()) 
-        return -1;
+        return 1;
     input = &finput;
     *input >> std::noskipws;
 
     std::ofstream foutput("../resources/result.html", std::ios::out);
     if (!foutput.good()) 
-        return -1;
+        return 1;
     output = &foutput;
+
+    OpenXLSX::XLDocument doc("../resources/test.xlsx");
+    if (doc.name() == "")
+        return 1;
+    auto wk = doc.workbook().worksheet("1");
+    size_t latter = 0;
+    while(wk.cell(latter + 1, 1).value().type() != OpenXLSX::XLValueType::Empty) {
+        dynamicMarks.push_back(std::vector<std::string>());
+        std::string current;
+        int number = 1;
+        while (wk.cell(latter + 1, number).value().type() != OpenXLSX::XLValueType::Empty) {
+            std::cerr << number << '\n';
+            std::string current = wk.cell(latter + 1, number).value().get<std::string>();
+            dynamicMarks[latter].push_back(std::move(current));
+            ++number;
+        }
+        ++latter;
+    }
+    doc.close();
+
 
     std::string file((std::istreambuf_iterator<char>(*input)), std::istreambuf_iterator<char>());
 
     for (count = 0; count < dynamicMarks.size(); ++count) {
         auto j = std::stringstream(file);
         j << std::noskipws;
-        if (replace(j, *output)) return -1;
+        if (replace(j, *output)) return 2;
     }
 
     return 0;
